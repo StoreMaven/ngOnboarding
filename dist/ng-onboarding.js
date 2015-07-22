254b2b1e515f048495c190414b417b1de50e932b
Zeev Novikov <zeev@storemaven.com>
2015-07-22 15:13:21 +0300

    next version

--- dist/ng-onboarding.js
+++ dist/ng-onboarding.js
@@ -6,16 +6,18 @@
 
   angular.module("ngOnboarding.html", []).run(["$templateCache", function($templateCache) {
     $templateCache.put("ngOnboarding.html",
-      "<div class='onboarding-container' ng-show='enabled'>\n" +
+      "<div class='onboarding-container' ng-show='isEnabled'>\n" +
       "<div class='{{overlayClass}}' ng-style='{opacity: overlayOpacity}', ng-show='overlay'></div>\n" +
-      "<div class='{{popoverClass}} {{positionClass}}' ng-style=\"{width: width, height: height, left: left, top: top, right: right, bottom: bottom}\">\n" + 
+      "<div ng-hide='waitForElement' ng-class='{ \"hide-popover\": waitForElement}' class='{{popoverClass}} {{positionClass}}' ng-style=\"{width: width, height: height, left: left, top: top, right: right, bottom: bottom}\">\n" + 
       "<div class='{{arrowClass}}'></div>\n    <h3 class='{{titleClass}}' ng-show='title' ng-bind='title'></h3>\n" +
       "<a href='' ng-click='close()' class='{{closeButtonClass}}' ng-bind-html='closeButtonText'></a>\n    <div class='{{contentClass}}'>\n" +
       "<p ng-bind-html='description'></p>\n    </div>\n    <div class='{{buttonContainerClass}}' ng-show='showButtons'>\n" +
       "<span ng-show='showStepInfo' class='{{stepClass}}'>{{actualStepText}} {{index + 1}} {{totalStepText}} {{stepCount}}</span>\n" +
-      "<a href='' ng-click='previous()' ng-show='showPreviousButton' class='{{buttonClass}}' ng-bind-html='previousButtonText'></a>\n" + 
-      "<a href='' ng-click='next()' ng-show='showNextButton' class='{{buttonClass}}' ng-bind-html='nextButtonText'></a>\n" +
-      "<a href='' ng-click='close()' ng-show='showDoneButton && lastStep' class='{{buttonClass}}' ng-bind-html='doneButtonText'></a>\n" +
+      "<a href='' ng-click='previous()' ng-show='showPreviousButton && showControlButtons' class='{{buttonClass}}' ng-bind-html='previousButtonText'></a>\n" + 
+      "<a href='' ng-click='next()' ng-show='showNextButton && showControlButtons' class='{{buttonClass}}' ng-bind-html='nextButtonText'></a>\n" +
+      "<a href='' ng-click='close()' ng-show='showDoneButton && lastStep && showControlButtons' class='{{buttonClass}}' ng-bind-html='doneButtonText'></a>\n" +
+      "<a href='' ng-click='next()' ng-show='showStepButton' class='{{buttonClass}} {{stepButtonClass}}' ng-bind-html='stepButtonText'></a>\n" +
+      "<a href='' ng-show='!lastStep' ng-click='close()' class='skip-onboarding' ng-bind-html='skipButtonText'></a>\n" + 
       "</div>\n  </div>\n</div>" +
       "");
   }]);
@@ -30,7 +32,7 @@
         titleClass: 'onboarding-popover-title',
         contentClass: 'onboarding-popover-content',
         arrowClass: 'onboarding-arrow',
-        buttonContainerClass: 'onboarding-button-container',
+        buttonContainerClass: 'onboarding-button-container maven',
         buttonClass: "onboarding-button",
         showButtons: true,
         nextButtonText: 'Next &rarr;',
@@ -42,6 +44,7 @@
         stepClass: 'onboarding-step-info',
         actualStepText: 'Step',
         totalStepText: 'of',
+        showControlButtons: false,
         showStepInfo: true
       },
       $get: function() {
@@ -68,7 +71,7 @@
       return {
         restrict: 'E',
         scope: {
-          enabled: '=',
+          isEnabled: '=',
           steps: '=',
           onFinishCallback: '=',
           index: '=stepIndex'
@@ -82,30 +85,37 @@
           attributesToClear = ['title', 'top', 'right', 'bottom', 'left', 'width', 'height', 'position'];
           scope.stepCount = scope.steps.length;
           scope.next = function() {
-            if( curStep.onNextCallback ){
-              curStep.onNextCallback();
+            if( curStep.onEnterCallback ){
+              curStep.onEnterCallback();
             }
+
+            if( scope.steps.length <= scope.index + 1 ){
+              return scope.close();
+            }
+
             return scope.index = scope.index + 1;
           };
           scope.previous = function() {
             return scope.index = scope.index - 1;
           };
-          scope.close = function() {
-            console.log('onboarding::close', scope, scope.onFinishCallback );
-            scope.enabled = false;
+          scope.close = function( noSkip ) {
+            noSkip = !!noSkip;
+            console.log('onboarding::close', scope, scope.onFinishCallback, noSkip );
+
+            scope.isEnabled = false;
             setupOverlay(false);
             if (scope.onFinishCallback) {
-              return scope.onFinishCallback();
+              return scope.onFinishCallback( !noSkip );
             }
           };
           scope.$watch('index', function(newVal, oldVal) {
-            console.log('onboarding::watch index', newVal, oldVal);
+            console.log('onboarding::watch index', newVal, oldVal, scope.isEnabled );
             if( !scope.steps.length ){
               return;
             }
             var attr, k, v, _i, _len;
             if (typeof newVal === 'undefined') {
-              scope.enabled = false;
+              scope.isEnabled = false;
               setupOverlay(false);
               return;
             }
@@ -137,6 +147,12 @@
             scope.actualStepText = $sce.trustAsHtml(scope.actualStepText);
             scope.totalStepText = $sce.trustAsHtml(scope.totalStepText);
 
+            scope.showStepButton  = curStep.nextButtonText && true;
+            scope.stepButtonClass  = curStep.stepButtonClass;
+            scope.stepButtonText  = curStep.nextButtonText;
+            scope.skipButtonText  = curStep.skipButtonText;
+
+            scope.waitForElement = true;
 
             //check if attachTo element is already exists on page
             var maxChecks=10, checkCounter=0, attachTo = curStep['attachTo'], checkForElement = function(){
@@ -150,32 +166,42 @@
                   checkForElement();
                 }, 1000 );
               }else{
-                setupOverlay();
-                setupPositioning();
+                scope.waitForElement = false;
+                $timeout( function(){
+                  setupOverlay();
+                  setupPositioning();
+                } );
               }
             };
 
-            checkForElement();
+            $timeout( function(){
+              checkForElement();
+            }, 1000);
 
           });
+
           setupOverlay = function(showOverlay) {
-            console.log('onboarding::setupOverlay', showOverlay );
-            if (showOverlay == null) {
+            console.log('onboarding::setupOverlay', showOverlay, scope.isEnabled, $(curStep['attachTo']), scope.overlay );
+            if (!angular.isDefined(showOverlay) || showOverlay == null) {
               showOverlay = true;
             }
+            
+            console.log('onboarding::setupOverlay', showOverlay, curStep['attachTo'], scope.overlay );
             $('.onboarding-focus').removeClass('onboarding-focus');
             if (showOverlay) {
               if (curStep['attachTo'] && scope.overlay) {
+                console.log('onboarding::setupOverlay', showOverlay, scope.isEnabled );
                 return $(curStep['attachTo']).addClass('onboarding-focus');
               }
             }
           };
+
           setupPositioning = function() {
             var attachTo, bottom, left, right, top, xMargin, yMargin;
             attachTo = curStep['attachTo'];
             scope.position = curStep['position'];
 
-            console.log('onboarding::setupPositioning', attachTo, curStep, $(attachTo).offset(),  $(attachTo).outerWidth() );
+            console.log('onboarding::setupPositioning', attachTo, scope.isEnabled, curStep, $(attachTo).offset(),  $(attachTo).outerWidth() );
 
             //console.log('onboarding::setupPositioning.actualSetUp', $(attachTo).offset(), $(attachTo).outerWidth() );
             xMargin = 15;
@@ -235,8 +261,13 @@
           }
 
           $rootScope.$on('onboarding.next', function( event, data ) {
-              console.log('onboarding.next event accepted', data );
+              console.log('onboarding.next event accepted', data, scope.index );
               scope.next();
+          });
+
+          $rootScope.$on('onboarding.close', function( event, data ) {
+              console.log('onboarding.close event accepted', data, scope.index );
+              scope.close( true );
           });
 
         },
